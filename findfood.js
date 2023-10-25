fa6af449f0946a81d834ce5ade1c0d1d8136f632
Ryan-Siglag <Ryan.Siglag@gmail.com>
2023-10-25 01:21:14 -0400

    Began API use

---
+++ findfood.js
@@ -0,0 +1,20 @@
+var dataValues = document.getElementById("data");
+
+dataValues.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var values = dataValues.elements;
+    console.log(values[0].value);
+
+    fetch('https://hook.us1.make.com/4yyx97ovmi6tf39iesal83m19lho5k04?zip=' + values[0].value, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        //console.log(data["Info"]);
+    })
+        
+});
