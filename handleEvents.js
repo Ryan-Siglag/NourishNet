b97c95888d723dd62ba3db08d1bcf82ef34a2614
Ryan-Siglag <Ryan.Siglag@gmail.com>
2023-10-31 17:00:50 -0400

    Clean Up

--- handleEvents.js
+++ handleEvents.js
@@ -1,6 +1,4 @@
 function addNewlinesBeforeNumbers(inputString) {
-    // Use a regular expression to find all numbers in the input string
-    // and add two newline characters before each number.
     var resultString = inputString.replace(/\d+/g, '<br><br>$&');
     return resultString;
   }
@@ -20,36 +18,17 @@ dataValues.addEventListener("submit", (e) => {
     })
     .then(response => response.json())
     .then(data => {
-        // for (var i = 0; i < data.; i++){
-        //     console.log(data[i]);
-        //     console.log("hi")
-        // }
         document.getElementById("foodbanks").style.display = "inline";
 
         for (const [key, value] of Object.entries(data)) {
-            //console.log(`${key}: ${value}`);
             document.getElementById(key).innerHTML = value;
-            
-            // "<a href=\'#\'>Test</a> "
-
           }
-        
-        //console.log(data["Info"]);
           
     })
     
     console.log(values)
     if (values[2].value != ""){
         document.getElementById("gptHolder").innerHTML = '<div class=\"gptInfo\"><p>Loading...</p></div>'
-        //console.log(values[2].value);
-        /*
-        fetch('https://hook.us1.make.com/xuejgwvk45i2x4buy6lkpo4ltc1wqmdh?State=' + values[2].value + '&Community=' + values[3].value,  + '&Demographic=' + values[4].value, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        */
         fetch('https://hook.us1.make.com/xuejgwvk45i2x4buy6lkpo4ltc1wqmdh?State=' + values[2].value + "&Community=" + values[3].value + "&Demographic=" + values[4].value , {
         method: 'POST',
         headers: {
