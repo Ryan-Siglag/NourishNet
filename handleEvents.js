788eadc732eb1d1753f07888d903bea6483c4f26
Ryan-Siglag <Ryan.Siglag@gmail.com>
2023-11-01 16:03:05 -0400

    bug fixes

--- handleEvents.js
+++ handleEvents.js
@@ -8,25 +8,25 @@ var dataValues = document.getElementById("data");
 dataValues.addEventListener("submit", (e) => {
     e.preventDefault();
     var values = dataValues.elements;
-    console.log(values[0].value);
-    
-    fetch('https://hook.us1.make.com/4yyx97ovmi6tf39iesal83m19lho5k04?zip=' + values[0].value, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("foodbanks").style.display = "inline";
 
-        for (const [key, value] of Object.entries(data)) {
-            document.getElementById(key).innerHTML = value;
-          }
-          
-    })
-    
     console.log(values)
+    
+    if (values[0].value != ""){
+        fetch('https://hook.us1.make.com/4yyx97ovmi6tf39iesal83m19lho5k04?zip=' + values[0].value, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("foodbanks").style.display = "inline";
+            for (const [key, value] of Object.entries(data)) {
+                document.getElementById(key).innerHTML = value;
+              } 
+        })
+    }
+    
     if (values[2].value != ""){
         document.getElementById("gptHolder").innerHTML = '<div class=\"gptInfo\"><p>Loading...</p></div>'
         fetch('https://hook.us1.make.com/xuejgwvk45i2x4buy6lkpo4ltc1wqmdh?State=' + values[2].value + "&Community=" + values[3].value + "&Demographic=" + values[4].value , {
@@ -37,16 +37,10 @@ dataValues.addEventListener("submit", (e) => {
         })
         .then(response => response.json())
         .then(data => {
-
             console.log(data["message"]);
             console.log(typeof data["message"]);
             var editedText = addNewlinesBeforeNumbers(data["message"]);
             document.getElementById("gptHolder").innerHTML = "<div class=\"gptInfo\"><p>" + editedText + "</p></div>";
-            
         })
         }
-        
 });
-
-//Change box for loading
-//Make embeds larger
